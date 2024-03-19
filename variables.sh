c34cb3a8c94b621f85cb01103953c9de87e29634
Ilia Javakhishvili <ilo@iMac.local>
2024-03-19 18:12:17 +0100

    Added variables to have them in my repo

---
+++ variables.sh
@@ -0,0 +1,10 @@
+export DB_USER=admin
+export DB_PWD=adminpass
+export DB_SERVER=mysql
+export DB_NAME=team-member-projects
+
+export MYSQL_ROOT_PASSWORD=rootpass
+
+export PMA_HOST=mysql
+export PMA_PORT=3306
+echo "done"
